20213c5c6090e75050ff349b0338d0bb13d0b659
김정환 <ej88ej@gmail.com>
2018-11-01 23:04:59 +0900

    mux

--- server.go
+++ server.go
@@ -2,18 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.Method, r.URL)
+	w.Write([]byte("hello world\n"))
+}
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(w, "<h2>Index page</h2>")
+}
+
 func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world\n"))
-	})
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", helloHandler)
+	r.HandleFunc("/", indexHandler)
+	http.Handle("/", r)
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html")
-		fmt.Fprintf(w, "<h2>Index page</h2>")
-	})
-
-	http.ListenAndServe(":3000", nil)
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
