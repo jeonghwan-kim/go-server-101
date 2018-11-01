a3b084ba416ac79c763197f8183015aa972727c6
김정환 <ej88ej@gmail.com>
2018-11-01 23:15:56 +0900

    json 응답 api 엔드포인트 추가

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 
@@ -12,6 +13,12 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.Method, r.URL)
 	w.Write([]byte("hello world\n"))
 }
+func bucketHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.Method, r.URL)
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	io.WriteString(w, `{"id": 1, "name": "my bucket 1"}`)
+}
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprintf(w, "<h2>Index page</h2>")
@@ -20,6 +27,7 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/hello", helloHandler)
+	r.HandleFunc("/api/buckets/{id}", bucketHandler)
 	r.HandleFunc("/", indexHandler)
 	http.Handle("/", r)
 
