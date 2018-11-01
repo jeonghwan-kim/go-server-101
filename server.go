fa269f06dc1946ff18aa5ae5f17a4c21b775ea5b
김정환 <ej88ej@gmail.com>
2018-11-01 22:45:38 +0900

    Index page

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -8,5 +9,11 @@ func main() {
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello world\n"))
 	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		fmt.Fprintf(w, "<h2>Index page</h2>")
+	})
+
 	http.ListenAndServe(":3000", nil)
 }
