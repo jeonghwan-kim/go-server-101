df52f753282358c9d39e979ff78bb4f3a43929bb
김정환 <6pack@madup.com>
2018-11-02 15:09:33 +0900

    Serve static files

--- server.go
+++ server.go
@@ -10,21 +10,11 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.Method, r.URL)
-	w.Write([]byte("hello world\n"))
-}
-
 func bucketHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.Method, r.URL)
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
 	io.WriteString(w, `{"id": 1, "name": "my bucket 1"}`)
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprintf(w, "<h2>Index page</h2>")
 }
 
 type Bucket struct {
@@ -48,12 +38,13 @@ func createBucketHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/hello", helloHandler)
-	r.HandleFunc("/api/buckets/{id}", bucketHandler)
-	r.HandleFunc("/api/buckets", createBucketHandler).Methods("POST")
-	r.HandleFunc("/", indexHandler)
-	http.Handle("/", r)
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/buckets/{id}", bucketHandler)
+	router.HandleFunc("/api/buckets", createBucketHandler).Methods("POST")
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./static")))
+
+	http.Handle("/", router)
 
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
